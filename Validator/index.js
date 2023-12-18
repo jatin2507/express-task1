3a7bc46c08abb0a2d86c4dede2ff9bd6d77a0206
jatin2507 <107983953+jatin2507@users.noreply.github.com>
2023-12-18 14:41:05 +0530

    Update index.js

--- Validator/index.js
+++ Validator/index.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 // Define the schema for validation
 const JoiSchema = Joi.object({
   name: Joi.string(),
-  age: Joi.number().integer().min(0).max(99),
+  sort: Joi.number().integer().min(0).max(99),
   userId: Joi.string().length(4),
 });
 
